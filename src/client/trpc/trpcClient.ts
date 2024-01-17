85ba58155b6ae23bbbc2cc484f17df3f38a9c336
Simon Baumgardt-Wellander <simonbw@uw.edu>
2024-01-17 13:01:26 -0800

    open websocket with matching security level

--- src/client/trpc/trpcClient.ts
+++ src/client/trpc/trpcClient.ts
@@ -1,7 +1,9 @@
 import { createTRPCProxyClient, createWSClient, wsLink } from "@trpc/client";
 import type { TrpcRouter } from "../../server/trpc/trpcRouter";
 
-const wsUrl = `ws://${window.location.host}/trpc`;
+const isSecure = window.location.protocol === "https:";
+
+const wsUrl = `${isSecure ? "wss" : "ws"}://${window.location.host}/trpc`;
 export const trpcClient = createTRPCProxyClient<TrpcRouter>({
   links: [
     wsLink({
